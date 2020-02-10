c8e4ea9f26cc9711a198739be6fad8e5fc38a027
League Student <league@iMac-14.attlocal.net>
2020-02-09 17:27:35 -0800

    e

--- src/_00_Sorting_Algorithms/BogoSorter.java
+++ src/_00_Sorting_Algorithms/BogoSorter.java
@@ -1,6 +1,8 @@
 package _00_Sorting_Algorithms;
 
+import java.sql.Time;
 import java.util.Random;
+import java.util.TimeZone;
 
 public class BogoSorter extends Sorter {
 	public BogoSorter() {
@@ -17,9 +19,42 @@ public class BogoSorter extends Sorter {
 	// STEP 2. Take two random elements in the array and swap them.
 	// STEP 3. Go back to step 1.
 	
-	//1. Complete the sort method using the Bogo sort algorithm. 
+	//1. Complete the sort method using the Bogo sort algorithm.
+	
+	
+	boolean inOrder(int[] array) {
+		int last = 0;
+		boolean InOrder = false;
+		for (int i = 0; i < array.length; i++) {
+			if(array[i] > last) {
+				InOrder = true;
+			}else {
+				InOrder = false;
+				return false;
+			}
+		}
+		return true;
+	}
+	
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
+		
+		Random rand = new Random();
+		int counter = 0;
+		while(inOrder(array)) {
+			int rand1 = rand.nextInt(array.length/2);
+			int rand2 = rand.nextInt(array.length/2);
+			if(array[rand1] > array[rand2]) {
+				int ram = array[rand2];
+				array[rand2] =  array[rand1];
+				array[rand1] = ram;
+			}
+			
+			counter++;
+			System.err.println("Itteration number "+counter);
+			display.updateDisplay();
+		}
+		
 
 	}
 }
--- src/_00_Sorting_Algorithms/BubbleSorter.java
+++ src/_00_Sorting_Algorithms/BubbleSorter.java
@@ -10,7 +10,21 @@ public class BubbleSorter extends Sorter{
 	//   progress on the graph.
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
-		
+		boolean sorted = false;
+		while(!sorted) {
+			sorted = true;
+			display.updateDisplay();
+
+			for (int i = 0; i < array.length-1; i++) {
+				if(array[i] > array[i+1]) {
+					int mem = array[i];
+					array[i] = array[i+1];
+					array[i+1] = mem;
+					sorted = false;
+				}
+			}
+			
+		}	
 	}
 	
 }
